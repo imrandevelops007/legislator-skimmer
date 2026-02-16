56098208c3ad29a7aa25a128e3ee034601d93620
Gideon Moerdyk <gideonmoerdyk@gmail.com>
2026-02-16 15:01:54 -0500

    Uncomment sample hub links print statement debug

--- main.py
+++ main.py
@@ -447,7 +447,7 @@ def main():
 
         try:
             links = collect_links_from_hub(hub)
-            #print("Sample hub links:", links[:10])
+            print("Sample hub links:", links[:10])
         except Exception as e:
             print(f"Failed to fetch/parse hub page: {e}")
             continue
